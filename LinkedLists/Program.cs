fc05894db26cb0876e04616ba942ef3d1271a38c
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-29 18:11:12 +0530

    [Biplab] Add: Deleting first Node

--- LinkedLists/Program.cs
+++ LinkedLists/Program.cs
@@ -10,9 +10,11 @@ namespace LinkedLists
             LinkedList list = new LinkedList();
             Console.WriteLine("Append the list");
             list.Append(56);
+            list.Append(30);
             list.Append(70);
             list.Display();
-            list.AddNodeAfterNode(30, 56);
+            Console.WriteLine("\nDelete the first node");
+            list.DeleteFirstNode();
             list.Display();
         }
     }
